2b34e039c1de2584817439f687c7569a447da4e5
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-05-30 10:01:34 -0300

    :tada: Commit inicial - Projeto GoLady

---
+++ src/main/java/com/generation/golady/GoLadyApplication.java
@@ -0,0 +1,13 @@
+package com.generation.golady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoLadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoLadyApplication.class, args);
+	}
+
+}
