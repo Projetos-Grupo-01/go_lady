0d539a242625afcebbd15228e05b1a911979993c
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-15 21:42:31 -0300

    :sparkles: Implementação do Spring Security e rota de Login

--- src/main/java/com/generation/golady/service/UsuarioService.java
+++ src/main/java/com/generation/golady/service/UsuarioService.java
@@ -1,22 +1,33 @@
 package com.generation.golady.service;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+
 import com.generation.golady.model.Usuario;
+import com.generation.golady.model.UsuarioLogin;
 import com.generation.golady.repository.UsuarioRepository;
+import com.generation.golady.security.JwtService;
 
 @Service
 public class UsuarioService {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
 
@@ -48,24 +59,40 @@ public class UsuarioService {
 	
 	}
 	
-	public String criptografarSenha(String senha) {
+	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
 		
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(senha.getBytes());
-
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-            
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Erro ao criptografar senha", e);
-        }
-    }
+		var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
+		
+		Authentication authentication = authenticationManager.authenticate(credenciais);
+		
+		if(authentication.isAuthenticated()) {
+			
+			Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
+			
+			if(usuario.isPresent()) {
+				usuarioLogin.get().setId(usuario.get().getId());
+				usuarioLogin.get().setNome(usuario.get().getNome());
+				usuarioLogin.get().setFoto(usuario.get().getFoto());
+				usuarioLogin.get().setSenha("");
+				usuarioLogin.get().setAvaliacao(usuario.get().getAvaliacao());
+				usuarioLogin.get().setTelefone(usuario.get().getTelefone());
+				usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
+				
+				return usuarioLogin;
+			}
+		}
+		
+		return Optional.empty();
+	}
+	
+	private String gerarToken(String usuario) {
+		return "Bearer " + jwtService.generateToken(usuario);
+	}
+	
+	private String criptografarSenha(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		return encoder.encode(senha);
+	}
 	
 }
